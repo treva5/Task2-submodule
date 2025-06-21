0f6b042a1f0ef95ddab0ea8184420fdfb0cba40f
treva5 <trevamukisa5@gamil.com>
2025-06-21 21:41:10 +0300

    branch 5: second commit

--- task2/main.c
+++ task2/main.c
@@ -4,20 +4,18 @@ int main() {
     float price, tax_rate, total_cost;
 
     // Assign a fixed tax rate (for example, 15%)
-    tax_rate = 0.1;
+    tax_rate = 0.15;
 
     // Input product price
-    printf("Enter product pre: ");
+    printf("Enter product price: ");
     scanf("%f", &price);
 
     // Calculate total cost including tax
-    total_cost = price +
+    total_cost = price + (tax_rate * price);
 
     // Display the total cost
     printf("Total Cost including tax = %.2f\n", total_cost);
+    system("pause");
 
     return 0;
 }
-
-
-
