d49f3929fe31481db857bef687c00c7069c436e9
treva5 <trevamukisa5@gamil.com>
2025-06-21 17:49:10 +0300

    branch 1: first commit

---
+++ task2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float principal, rate, time, interest;
+
+    // Input principal, rate, and time
+    printf("Enter principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter interest rate (in percentage): ");
+    scanf("%f", &rate);
+
+    ;
+
+    return 0;
+}
