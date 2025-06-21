d774fe8fa011812419dda50b749d51d4c14f2b20
treva5 <trevamukisa5@gamil.com>
2025-06-21 20:48:57 +0300

    branch 3: second commit

--- task2/main.c
+++ task2/main.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 
 
-
 int main() {
-     total_ugx;
+    float rate_per_usd = 3500.0; // UGX needed for 1 USD
+    float usd_amount = 700.0;    // USD amount to exchange
+    float total_ugx;
 
     // Calculate total UGX needed
     total_ugx = rate_per_usd * usd_amount;
@@ -12,11 +13,8 @@ int main() {
     // Display the result
     printf("UGX needed to buy %.2f USD: %.2f\n", usd_amount, total_ugx);
 
-
-
-    system("pause");
-
-
     return 0;
 }
+
+
 
